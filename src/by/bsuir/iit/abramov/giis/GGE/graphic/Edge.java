aa1966e2c3b3db6f80755882f6411031679f5356
Pavel Abramov <abramovpav@gmail.com>
2014-09-26 13:46:31 +0300

    move special algorithm part to EdgeDDA class. Edge use standart drawLine method

--- src/by/bsuir/iit/abramov/giis/GGE/graphic/Edge.java
+++ src/by/bsuir/iit/abramov/giis/GGE/graphic/Edge.java
@@ -72,47 +72,10 @@ public class Edge extends JComponent implements GraphicObject {
 		Graphics2D g2d = (Graphics2D)g;
 		Point firstPoint = getFirstPointOnCanvas();
 		Point lastPoint = getLastPointOnCanvas();
-		drawLine(g2d, firstPoint.getX(), firstPoint.getY(), lastPoint.getX(), lastPoint.getY());
+		g2d.drawLine(firstPoint.getX(), firstPoint.getY(), lastPoint.getX(), lastPoint.getY());
 	}
 	
-	private void drawLine(Graphics2D g2d, int x1, int y1, int x2, int y2) {
-
-		int length = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1));
-		double dx = (double)(x2 - x1) / length;
-		double dy = (double)(y2 - y1) / length;
-		double x = x1 + 0.5 * sign(dx);
-		double y = y1 + 0.5 * sign(dy);
-		drawPoint(g2d, x, y);
-		int i = 0;
-		while(i < length) {
-			x = x + dx;
-			y = y + dy;
-			drawPoint(g2d, x, y);
-			i += 1;
-		}
-	}
-	
-	private void drawPoint(Graphics2D g2d, double x, double y) {
-		g2d.drawLine((int)x, (int)y, (int)x, (int)y);
-	}
-	
-	private int sign(double x) {
-		if (x == 0) {
-			return 0;
-		}
-		else if (x > 0) {
-			return 1;
-		}
-		else {
-			return -1;
-		}
-	}
-	
-//	private int getLength(int x1, int y1, int x2, int y2) {
-//		return 
-//	}
-	
-	private Point getLastPointOnCanvas() {
+	protected Point getLastPointOnCanvas() {
 		Point point = new Point();
 		Point refPoint = getRefferencepoint();
 		point.setX(endPoint.getX() - refPoint.getX());
@@ -120,7 +83,7 @@ public class Edge extends JComponent implements GraphicObject {
 		return point;
 	}
 	
-	private Point getFirstPointOnCanvas() {
+	protected Point getFirstPointOnCanvas() {
 		Point point = new Point();
 		Point refPoint = getRefferencepoint();
 		point.setX(startPoint.getX() - refPoint.getX());
---
+++ src/by/bsuir/iit/abramov/giis/GGE/graphic/EdgeDDA.java
@@ -0,0 +1,48 @@
+package by.bsuir.iit.abramov.giis.GGE.graphic;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class EdgeDDA extends Edge {
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		Point firstPoint = getFirstPointOnCanvas();
+		Point lastPoint = getLastPointOnCanvas();
+		drawLine(g2d, firstPoint.getX(), firstPoint.getY(), lastPoint.getX(), lastPoint.getY());
+	}
+	
+	private void drawLine(Graphics2D g2d, int x1, int y1, int x2, int y2) {
+
+		int length = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1));
+		double dx = (double)(x2 - x1) / length;
+		double dy = (double)(y2 - y1) / length;
+		double x = x1 + 0.5 * sign(dx);
+		double y = y1 + 0.5 * sign(dy);
+		drawPoint(g2d, x, y);
+		int i = 0;
+		while(i < length) {
+			x = x + dx;
+			y = y + dy;
+			drawPoint(g2d, x, y);
+			i += 1;
+		}
+	}
+	
+	private void drawPoint(Graphics2D g2d, double x, double y) {
+		g2d.drawLine((int)x, (int)y, (int)x, (int)y);
+	}
+	
+	private int sign(double x) {
+		if (x == 0) {
+			return 0;
+		}
+		else if (x > 0) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+}
